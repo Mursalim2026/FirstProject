0378e2178893e755c4a3ba9352cb20c92ba8b08c
mursalim <msk93751@gmail.com>
2025-01-22 19:55:06 +0530

    new code added

---
+++ script.js
@@ -0,0 +1,61 @@
+/*console.log("mursalim");
+var mi ="hello world"; 
+console.log(mi.charAt(mi.length-1));
+var a = "hello";
+var b = "world";
+var c = b.concat(a);
+console.log(c);
+console.log(a.indexOf('1'));
+let arr =[ 10,20,[67,90,0], "hello", true, false ]
+console.log(arr[2][3]);
+let obl={
+name: "mursalim",
+class: 5 , 
+INVHJHH:true
+
+};
+console.log(obl.name);
+let man = 4;
+if( 0<man>4){
+console.log("number is creat");
+
+}
+else(5<man>10){
+    console.log("number is rong");
+}*/
+    console.log("mursalim")
+let name = function (){
+console.log("nihaj mursalim  akter");
+}
+name();
+name();
+name();
+name();
+name();
+function fa(){
+    console.log("hi are you ok?");
+}
+function fs (yya){
+    console.log("yes i am ok");
+    yya();
+}
+fs(fa);
+function f1 (uio){
+    console.log("hi mursalim");
+    (uio);
+}
+function f2 (bnm){
+    console.log("yes i am mursalim");
+    let newf = () => {
+        console.log(" ok byyy")
+    }
+bnm(newf);
+}
+f2(f1);
+
+
+
+
+
+
+
